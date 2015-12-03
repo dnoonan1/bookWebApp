8ee3a0f985363d51b46ec100f956986bf90fc69c
dnoonan1 <dnoonan1@L11107042788.wctc.msft>
2015-12-02 21:09:41 -0600

    27th commit
    - added REST HATEOAS

--- src/main/java/edu/wctc/drn/bookwebapp/repository/AuthorRepository.java
+++ src/main/java/edu/wctc/drn/bookwebapp/repository/AuthorRepository.java
@@ -3,11 +3,13 @@ package edu.wctc.drn.bookwebapp.repository;
 import edu.wctc.drn.bookwebapp.entity.Author;
 import java.io.Serializable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  *
  * @author Dan
  */
+@RepositoryRestResource(collectionResourceRel = "authors", path = "authors")
 public interface AuthorRepository extends JpaRepository<Author, Integer>, Serializable {
     
 }
